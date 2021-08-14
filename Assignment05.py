6064b0f4d41bf122b6ab4f04d03b5bec8192c627
Kristen <88906780+kristenrb@users.noreply.github.com>
2021-08-14 15:17:59 -0700

    Update Assignment05.py

--- Assignment05.py
+++ Assignment05.py
@@ -11,7 +11,9 @@
 
 # -- Data -- #
 # declare variables and constants
-objFile = "ToDoList.txt"   # An object that represents a file
+from typing import TextIO
+
+objFile = open("ToDoList.txt", "w")   # An object that represents a file, creates new file
 strData = ""  # A row of text data from the file
 dicRow = {}    # A row of data separated into elements of a dictionary {Task,Priority}
 lstTable = []  # A list that acts as a 'table' of rows
@@ -24,8 +26,8 @@ strPriority = "" # A priority entered by user
 # -- Processing -- #
 # Step 1 - When the program starts, load the any data you have
 # in a text file called ToDoList.txt into a python list of dictionaries rows (like Lab 5-2)
-objFile = open("ToDoList.txt", "r")
-for strData in objFile:
+objFileToDo = open("ToDoList.txt", "r")
+for strData in objFileToDo:
     strTask = strData.split(",")[0].strip()
     strPriority = strData.split(",")[1].strip()
     dicRow = {"Task": strTask, "Priority": strPriority}
@@ -33,7 +35,7 @@ for strData in objFile:
 objFile.close()
 
 # # -- Input/Output -- #
-# # Step 2 - Display a menu of choices to the user
+# Step 2 - Display a menu of choices to the user
 while (True):
     print("""
     Menu of Options
@@ -46,7 +48,7 @@ while (True):
     strChoice = str(input("Which option would you like to perform? [1 to 5] - "))
     print()  # adding a new line for looks
 
-#     # Step 3 - Show the current items in the table
+# Step 3 - Show the current items in the table
     if (strChoice.strip() == '1'):
         if not lstTable:
             print("To do list is empty! Enjoy your day or add new items.")
@@ -54,7 +56,7 @@ while (True):
         else:  # if lstTable has data, do the follow:
             print("Current items on To-Do List:")
             print()
-            for dicRow in lstTable:  # loop through lstTable and print row data
+            for dicRow in lstTable:  # loop through Table and print row data
                 for Task, Priority in dicRow.items():
                     print(Task, " : ", Priority)
         print() # new line for looks
@@ -62,17 +64,16 @@ while (True):
 
 # Step 4 - Add a new item to the list/Table
     elif (strChoice.strip() == '2'):
-        strTask = str(input("Enter a new task: "))
-        strPriority = str(input("Enter a priority level (1 to 5): "))
+        strTask = input("Enter a new task: ")
+        strPriority = input("Enter a priority level (1 to 5): ")
         print("You entered: " + strTask + " as priority level " + strPriority)
-        dicRow = {"Task": strTask, "Priority": strPriority}
-        lstTable.append(dicRow)
+        dicRow = {"Task": strTask, "Priority": strPriority} # adds the input to the dictionary
+        lstTable.append(dicRow) # adds row to table
         continue
-
 
 # Step 5 - Remove a new item from the list/Table
     elif (strChoice.strip() == '3'):
-        strTask = str(input("Which item would you like to remove? (Type BACK to return to menu):  "))
+        strTask = input("Which item would you like to remove? (Type BACK to return to menu):  ")
         for row in lstTable:
             if row["Task"] == strTask:
                 lstTable.remove(row)
@@ -81,7 +82,7 @@ while (True):
             continue
         continue
 
-# Step 6 - Save tasks to the ToDoToDoList.txt file
+# Step 6 - Save tasks to the ToDoList.txt file
     elif (strChoice.strip() == '4'):
         objFile = open("ToDoList.txt", "w")
         for row in lstTable:
@@ -94,3 +95,4 @@ while (True):
     elif (strChoice.strip() == '5'):
         input("Goodbye!")
         break  # and Exit the program
+
